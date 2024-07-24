165f03c5275de88a7b7464b76a2d0d99925720e0
CharlesDouxPro <charles.doux.pro@gmail.com>
2024-07-24 15:33:06 +0200

    simple page architecture

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,26 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div>
-      <h1>Bonjours</h1>
+  <div id="app">
+    <header class="b1">
+      <h1>salut</h1>
+    </header>
+    <div class="b2 d-flex flex-row">
+      <div class="b3 f1 content-center">
+        <p>navbar</p>
+      </div>
+      <div class="b3 d-flex flex-col min-h-100 f9">
+        <div class="b4 f5 content-center"><h1>oui</h1></div>
+        <div class="b5 f5 content-center"><h1>Non</h1></div>
+      </div>
     </div>
-  </header>
+    <footer class="b4">
+      <h2>je suis un pied</h2>
+    </footer>
+  </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 @use '@/assets/style.scss';
 @use '@/assets/debug.scss';
 </style>
